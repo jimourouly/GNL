4d6b78c1bc52047d551df512ea39c93d93a8e884
Jimmy Roulet <jroulet@c1r3s1.42lausanne.ch>
2023-12-09 21:06:29 +0100

    added mod

--- get_next_line.h
+++ get_next_line.h
@@ -6,22 +6,37 @@
 /*   By: jroulet <marvin@42lausanne.ch>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/09 16:54:37 by jroulet           #+#    #+#             */
-/*   Updated: 2023/12/09 17:13:40 by jroulet          ###   ########.fr       */
+/*   Updated: 2023/12/09 20:51:26 by jroulet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE
 # define GET_NEXT_LINE
-# include <stlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <stdlib.h>
 # include <unistd.h>
 
 char *get_next_line(int fd);
 
 typedef struct s_node{
-	char 			buf[BUFFER_SIZE + 1];
+	char 			node_buff[BUFFER_SIZE + 1];
 	struct s_node	*next;
 }t_node;
 
+void ft_onfreetoutca(t_node **head, t_node *new_node, char *buff);
+int ft_length_node(t_node *head);
+int ft_found_newligne(t_node *head);
+t_node *ft_find_last_node(t_node *head);
+void	ft_node_insert_to_end(t_node **head, char *buff);
+void	ft_create_list(t_node **list, int fd);
+void ft_copy_node(t_node *head, char *str);
+char *ft_create_line(t_node *head);
+void ft_cleaning(t_node **head);
+char *get_next_line(int fd);
 
 
 #endif
