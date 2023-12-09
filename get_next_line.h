63ade84efb4653ea70ef122c7a2b76973aa23564
Jimmy Roulet <jroulet@c1r13s1.42lausanne.ch>
2023-12-09 17:29:53 +0100

    mod

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: jroulet <marvin@42lausanne.ch>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/09 16:54:37 by jroulet           #+#    #+#             */
-/*   Updated: 2023/12/09 17:06:50 by jroulet          ###   ########.fr       */
+/*   Updated: 2023/12/09 17:13:40 by jroulet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 char *get_next_line(int fd);
 
 typedef struct s_node{
-	char 			/*buffersize*/;
+	char 			buf[BUFFER_SIZE + 1];
 	struct s_node	*next;
 }t_node;
 
