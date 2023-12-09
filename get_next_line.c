155ae59ccd42784a660978b6b3021ababce5d55b
Jimmy Roulet <jroulet@c1r3s1.42lausanne.ch>
2023-12-09 18:35:16 +0100

    added read

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,44 @@
 /*   By: jroulet <marvin@42lausanne.ch>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/09 16:54:26 by jroulet           #+#    #+#             */
-/*   Updated: 2023/12/09 17:17:25 by jroulet          ###   ########.fr       */
+/*   Updated: 2023/12/09 18:33:17 by jroulet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
-
+//#include "get_next_line.h"
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+/*
 char *get_next_line(int fd)
 {
+	static t_node	*node = NULL;
+	t_node			*head;
+	char			*newligne;
+	ssize_t			ride;
 
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+
+}*/
+
+int main (void)
+{
+	char buff[5];
+	int fd;
+	int i;
+	char *filename;
+	int char_read;
+	ssize_t lire;
+
+	filename = "file.txt";
+	fd = open(filename, O_RDONLY);
+	
+	while((char_read = read(fd, buff,5)))
+	{
+		buff[char_read] = '\0';
+		printf("buff\t%d = :%s:\n",i,buff);
+		i++;
+	}
 }
